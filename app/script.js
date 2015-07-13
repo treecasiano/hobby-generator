59b713af7e02cc52afedaba632b7a44a0288558d
Tree Casiano <tree.pdx@gmail.com>
2015-07-13 13:37:25 -0700

    Initial commit.

--- app/script.js
+++ app/script.js
@@ -1,36 +1,3 @@
-$('#piglatin').on( 'submit', function() {
-  var request = {
-     text: $( '#to-translate' ).val(),
-  };
+var hobbies = document.getElementById('hobbies');
 
-
-$.post('translate', request, function(response) {
-  var piglatinified = response.piglatin;
-  $("#translated").text( response.piglatin );
-});
-
-  console.log(request);
-
-  return false; // this keeps the page from refreshing
-
-});
-
-
-$('#farfallino').on( 'submit', function() {
-  var request = {
-     text: $( '#to-translate-farfallino' ).val(),
-  };
-
-
-$.post('farfallino', request, function(response) {
-  var farfallinofied = response.farfallino;
-  $("#translated-farfallino").text( response.farfallino );
-});
-
-  console.log(request);
-
-  return false; // this keeps the page from refreshing
-
-});
-
-
+console.log(hobbies);
