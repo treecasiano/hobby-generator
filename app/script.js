b5586aea181a7ce07fde7c572e318997e6c7ff1e
Tree Casiano <tree.pdx@gmail.com>
2015-07-13 15:00:25 -0700

    Correct click function.

--- app/script.js
+++ app/script.js
@@ -1,3 +1,7 @@
-var hobbies = document.getElementById('hobbies');
-
-console.log(hobbies);
+$('#hobbies').click(function() {
+  console.log("Is this thing on?");
+  $.post('hobbies', function(response) {
+    var generatedHobby = response.hobbies;
+    $("#hobbies").text( response.hobbies );
+    });
+});
