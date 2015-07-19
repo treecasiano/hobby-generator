5d913ce90a7f3ac4607297846b95eea305dd6cef
Tree Casiano <tree.pdx@gmail.com>
2015-07-18 17:03:41 -0700

    Remove console.log.

--- app/script.js
+++ app/script.js
@@ -1,5 +1,4 @@
 $('#hobbies').click(function() {
-  console.log("Is this thing on?");
   $.post('hobbies', function(response) {
     var generatedHobby = response.hobbies;
     $("#hobbies").text( response.hobbies );
