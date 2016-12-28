77901725f88adb3207475076c242835c05e820ec
Brian <hab@ochin.org>
2016-12-28 09:43:54 -0800

    Add bower static files to load.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+'use strict';
 var express = require('express');
 var app = express();
 var hobbyGenerator = require ('./lib/hobbies');
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(3006, function () {
   var host = server.address().address;
   var port = server.address().port;
 
@@ -10,6 +11,7 @@ var server = app.listen(process.env.PORT || 3000, function () {
 });
 
 app.use(express.static('app'));
+app.use(express.static('static'));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
