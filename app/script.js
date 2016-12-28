e7dcd1e51c9eb373545003e8eb3f2d41d639113b
Brian <hab@ochin.org>
2016-12-28 09:42:56 -0800

    Render html in react.

--- app/script.js
+++ app/script.js
@@ -1,8 +1,30 @@
-"use strict";
+'use strict';
 
-$('#hobbies').click(function () {
-    $.post('hobbies', response => {
-        let generatedHobby = response.hobbies;
-        $("#hobbies").text(generatedHobby);
-    });
-});
+class Generator extends React.Component {
+    render() {
+        return (
+            <button id="hobbies" class="hobby-button" type="button" className="generator" onClick={() =>{}}>
+                CLICK TO GENERATE A HOBBY
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <h1 class="page-header">Hobby Generator</h1>
+        <p>Not sure what to do with your free time? Don't let your lack of imagination thwart your ability to lead a rich and
+        interesting life.</p>
+
+        <p>Let the Hobby Generator suggest one or several of the myriad ways you can spend your fleeting, finite time on this
+        terrible, beautiful planet!</p>
+        <Generator />
+        <br />
+        <br />
+        <footer class="main-footer">
+            <p>&copy; <a href="http://treecasiano.com">Tree Casiano</a> 2015</p>
+            <p>View <a href="https://github.com/treecasiano/hobby-generator">this code on GitHub!</a></p>
+        </footer>
+    </div>,
+  document.getElementById('app')
+);
