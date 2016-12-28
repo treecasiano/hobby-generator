7d1c6883742fcca1df2d730d927b07dcf5e242e5
Brian <hab@ochin.org>
2016-12-28 11:42:12 -0800

    Add http request for hobby.

--- app/script.js
+++ app/script.js
@@ -1,34 +1,28 @@
 'use strict';
 
-let hobbiesButton = document.getElementById('hobbies');
-
-hobbiesButton.addEventListener('click', () => {
-    getHobby();
-}, false);
-
-function getHobby() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'hobbies');
-    xhr.send(null);
-    xhr.onreadystatechange = () => {
-        const DONE = 4; // readyState 4 means the request is done
-        const OK = 200; // status 200 is a successful return
-        if (xhr.readyState === DONE) {
-            if (xhr.status === OK && xhr.status < 300) {
-                let responseObj = JSON.parse(xhr.responseText);
-                hobbiesButton.innerText = responseObj.hobbies;
-            } else {
-                console.log('Error: ' + xhr.status); // An error occurred during the request.
-            }
-        }
-    }
-}
-
 class Generator extends React.Component {
+      constructor() {
+            super();
+            this.state = {value: "CLICK TO GENERATE A HOBBY"};
+        }
     render() {
         return (
-            <button id="hobbies" class="hobby-button" type="button" className="generator" onClick={() =>{}}>
-                CLICK TO GENERATE A HOBBY
+            <button id="hobbies" class="hobby-button" type="button" className="generator"  onClick={() =>{let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'hobbies');
+        xhr.send(null);
+        xhr.onreadystatechange = () => {
+            const DONE = 4; // readyState 4 means the request is done
+            const OK = 200; // status 200 is a successful return
+            if (xhr.readyState === DONE) {
+                if (xhr.status === OK && xhr.status < 300) {
+                    let responseObj = JSON.parse(xhr.responseText);
+                    this.setState({value:responseObj.hobbies});
+                } else {
+                    console.log('Error: ' + xhr.status); // An error occurred during the request.
+                }
+            }
+        };}}>
+                {this.state.value}
             </button>
         );
     }
