230913c8af34bda4fc1269025215d54282308d3d
Tree Casiano <tree.pdx@gmail.com>
2016-12-30 13:13:19 -0800

    Refactor logic to randomize hobbies.

--- app/script.jsx
+++ app/script.jsx
@@ -2,117 +2,99 @@
 
 /* global React ReactDOM */
 
-let hobbiesList = [];
-let tempHobbiesList = [];
+class HobbyGenerator extends React.Component {
 
-postRequest('/hobbies')
-    .then((response) => {
-        // console.log('Success!', response);
-        let data = JSON.parse(response);
-        hobbiesList = data.hobbies;
-        tempHobbiesList = hobbiesList.slice(0);
-    }, error => {
-        console.log('Failed!', error);
-    });
+    constructor(props) {
 
-// TODO(Tree): create a stateful component to hold the text display and the button
+        super(props);
 
-class HobbyTextDisplay extends React.Component {
-    constructor() {
-        super();
+        this.hobbiesList = [];
+
         this.state = {
-            value: "CLICK BUTTON TO GENERATE A HOBBY",
-        };
-    }
-
-    render() {
-        return (
-            <div id="hobbyText" className="hobbyTextDisplay">
-                {this.state.value}
-            </div>
-        );
-    }
-}
-
-class GeneratorButton extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            value: "CLICK ME",
-        };
-        this.handleClick = this.handleClick.bind(this);
+            tempHobbiesList: [],
+            currentHobby: 'Click button below to generate a hobby.'
+        }
     }
 
     handleClick() {
-        let randomHobby = getRandomHobby();
-        let hobbyText = document.getElementById('hobbyText');
-        hobbyText.innerText = randomHobby;
+
+        let currentHobby = this.state.tempHobbiesList[Math.floor(Math.random() * this.state.tempHobbiesList.length)];
+        let index = this.state.tempHobbiesList.indexOf(currentHobby);
+        if (index > -1) {
+            this.state.tempHobbiesList.splice(index, 1);
+            this.setState({currentHobby});
+        } else {
+            // starts the list over
+            let currentHobby = "HOBBY GENERATOR IS EXHAUSTED. CLICK BUTTON TO START OVER.";
+            this.state.tempHobbiesList = this.hobbiesList.slice(0);
+            this.setState({currentHobby});
+        }
     }
-    
+
+    componentDidMount() {
+
+        postRequest('/hobbies')
+            .then((response) => {
+                const data = JSON.parse(response);
+                this.hobbiesList = data.hobbies;
+                this.state.tempHobbiesList = data.hobbies.slice(0);
+            }, error => {
+                console.log('Failed!', error);
+            });
+
+
+        function postRequest(url) {
+
+            return new Promise(function (resolve, reject) {
+                let req = new XMLHttpRequest();
+                req.open('POST', url);
+
+                req.onload = function () {
+
+                    if (req.status == 200) {
+                        resolve(req.response);
+                    }
+                    else {
+                        reject(Error(req.statusText));
+                    }
+                };
+
+                // Handle network errors
+                req.onerror = function () {
+                    reject(Error("Network Error"));
+                };
+
+                // Make the request
+                req.send();
+            });
+        }
+    }
+
     render() {
         return (
-            <button id="hobbies" type="button" className="generatorButton"  onClick={this.handleClick}>
-                {this.state.value}
-            </button>
-        );
+            <div className="hobbyGeneratorContainer">
+                <div id="hobbyText" className="hobbyTextDisplay">
+                    {this.state.currentHobby}
+                </div>
+                <button id="hobbies" type="button" className="generatorButton" onClick={this.handleClick.bind(this)}>
+                    Get Hobby
+                </button>
+            </div>
+        )
     }
 }
-
-function postRequest(url) {
-    // Return a new promise.
-    return new Promise(function(resolve, reject) {
-        // Do the usual XHR stuff
-        let req = new XMLHttpRequest();
-        req.open('POST', url);
-
-        req.onload = function() {
-            // This is called even on 404 etc
-            // so check the status
-            if (req.status == 200) {
-                // Resolve the promise with the response text
-                resolve(req.response);
-            }
-            else {
-                // Otherwise reject with the status text
-                // which will hopefully be a meaningful error
-                reject(Error(req.statusText));
-            }
-        };
-
-        // Handle network errors
-        req.onerror = function() {
-            reject(Error("Network Error"));
-        };
-
-        // Make the request
-        req.send();
-    });
-}
-
-function getRandomHobby() {
-    let hobby = tempHobbiesList[Math.floor(Math.random()*tempHobbiesList.length)];
-    let index = tempHobbiesList.indexOf(hobby);
-    if (index > -1) {
-        tempHobbiesList.splice(index, 1);
-    } else {
-        // starts the list over
-        tempHobbiesList = hobbiesList.slice(0);
-        return "RESTARTING THE LIST";
-    }
-    return hobby;
-}
-
 
 ReactDOM.render(
     <div>
         <h1 className="page-header">Hobby Generator</h1>
-        <p>Not sure what to do with your free time? Don't let your lack of imagination thwart your ability to lead a rich and
-        interesting life.</p>
+        <p>Not sure what to do with your free time? Don't let your lack of imagination thwart your ability to lead a
+            rich and
+            interesting life.</p>
 
-        <p>Let the Hobby Generator suggest one or several of the myriad ways you can spend your fleeting, finite time on this
-        terrible, beautiful planet!</p>
-        <HobbyTextDisplay />
-        <GeneratorButton />
+        <p>Let the Hobby Generator suggest one or several of the myriad ways you can spend your fleeting, finite time on
+            this
+            terrible, beautiful planet!</p>
+        <HobbyGenerator />
         <br />
         <br />
         <footer className="main-footer">
